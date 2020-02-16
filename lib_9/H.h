aec0675ca49c798da7283f2e909f99bd6e5450b0
IDA 102 <DarkAngel-green@rambler.ru>
2020-02-16 22:50:38 +0300

    START

---
+++ lib_9/H.h
@@ -0,0 +1,27 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define	  stop __asm nop
+#pragma warning(disable: 4786)
+//#include <typeinfo>
+#include <tchar.h>
+#include <string.h>
+#include <cstdio>
+#include <conio.h>
+#include <fstream>
+#include <iostream>
+#include <Windows.h>
+#include <string>
+//#include <stdexcept>
+#include <vector>
+#include <list>
+#include <deque>
+
+//#include "stdafx.h"
+#include <stack>
+#include <set>
+#include <map>
+#include <clocale>
+using namespace std;
+//#include "MyString.h"
+//#include "Point.h"
+//#include "ContOut.h"
